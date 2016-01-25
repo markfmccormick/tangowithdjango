b02a1dc711b20d2b80effff5f8725d529718fde8
unknown <2137329m@BO706-54.ad.dcs.gla.ac.uk>
2016-01-25 14:53:07 +0000

    Latest Code

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -14,5 +14,6 @@ def index(request):
 
     return render(request, 'rango/index.html', context_dict)
 
+
 def about(request):
     return HttpResponse('Rango says here is the about page. <a href="/rango/">Index</a>')
