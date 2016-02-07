b471d97c1f0bef008a117fd165e74dc2d9b75a5a
Mark McCormick <markmccormick@Marys-Air.home>
2016-02-07 21:33:53 +0000

    End of Chapter 9, some exercises don't work fully.

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -12,15 +12,18 @@ def populate():
 
     add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",
+        views=10)
 
     add_page(cat=python_cat,
         title="How to Think like a Computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=20)
 
     add_page(cat=python_cat,
         title="Learn Python in 10 Minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=23)
 
     python_cat.views=128
     python_cat.likes=64
@@ -29,15 +32,18 @@ def populate():
 
     add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/")
+        url="https://docs.djangoproject.com/en/1.5/intro/tutorial01/",
+        views=4)
 
     add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=2)
 
     add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",
+        views=500)
 
     django_cat.views=64
     django_cat.likes=32
@@ -46,14 +52,17 @@ def populate():
 
     add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottlepy.org/docs/dev/")
+        url="http://bottlepy.org/docs/dev/",
+        views=24)
 
     add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",
+        views=23)
 
     frame_cat.views=32
     frame_cat.likes=16
+
 
     # Print out what we have added to the user.
     for c in Category.objects.all():
